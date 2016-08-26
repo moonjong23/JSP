a505e10209a89f1e62ba4cb0d52da580cf3dfa37
mj <fightboolo@gmail.com>
2016-08-26 19:47:50 +0900

    8/26 톰캣 재설치...

--- sweb/src/pack/SessionTest.java
+++ sweb/src/pack/SessionTest.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import jdk.nashorn.internal.ir.RuntimeNode.Request;
-
 @WebServlet("/SessionTest")
 public class SessionTest extends HttpServlet {
 	
